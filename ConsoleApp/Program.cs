9fa382c117b1d4691793d0a9e8a9568674b08bca
Olya <belyaevaolga881@gmail.com>
2021-11-24 00:40:20 +0300

    simple db

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,28 +2,53 @@
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using ModelLibrary;
+using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace ConsoleApp
 {
+    public class DetectedObject
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Path { get; set; }
+    }
+
+    class UserContext : DbContext
+    {
+        public UserContext() : base("DbConnection") { }
+
+        public DbSet<DetectedObject> DetectedObjects { get; set; }
+    }
+
     class Program
     {
         private static async Task Consumer()
         {
-            string print;
-            while (true)
+            string type;
+            string image;
+
+            using (UserContext db = new UserContext())
             {
-                print = await Detector.bufferBlock.ReceiveAsync();
-                if (print == "end")
-                    break;
-                Console.WriteLine(print);
+                while (true)
+                {
+                    (type, image) = await Detector.resultBufferBlock.ReceiveAsync();
+                    if (type == "end")
+                    {
+                        db.SaveChanges();
+                        break;
+                    }
+                    DetectedObject result = new DetectedObject { Type = type, Path = image };
+                    db.DetectedObjects.Add(result);
+                }
+
+                var detectedObjects = db.DetectedObjects;
+                Console.WriteLine("Список объектов:");
+                foreach (DetectedObject dobj in detectedObjects)
+                {
+                    Console.WriteLine("{0} - {1}", dobj.Type, dobj.Path);
+                }
             }
-            
-            /*
-            while (await Detector.bufferBlock.TryReceive(out string value))
-            {
-                Console.WriteLine(value);
-            }
-            */
         }
         static async Task Main()
         {
